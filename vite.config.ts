897dab73b48d8fdfba3cfa1b20ddd07b1d0b7b36
abdullah ashraf <alpha1.abdullah@gmail.com>
2024-12-16 03:05:26 +0200

    added: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/My-Resume/', // Correctly set to your repository name
+  base: '/My-Resume/' // Replace 'My-Resume' with your GitHub repository name
 });
